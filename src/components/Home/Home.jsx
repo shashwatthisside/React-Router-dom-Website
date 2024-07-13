97d24297bf745c51f9d238ea517cb69f1919fb1e
shashwat Dwivedi <itsshashwatdwivedi@gmail.com>
2024-07-14 00:51:13 +0530

    all changes has done

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+    return (
+        <div className="mx-auto w-full max-w-7xl">
+            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+                <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                    <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+                        <h2 className="text-4xl font-bold sm:text-5xl">
+                            My name is Shashwat
+                            <span className="hidden sm:block text-4xl mt-4">A front-End Website Developer</span>
+                        </h2>
+
+                        <Link
+                            className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
+                            to="/"
+                        >
+                            <div>
+                                <button> <a href="https://www.linkedin.com/in/shashwat-dwivedi-563a511aa/">
+                                    Linkedin Profile
+                                </a>
+                                </button>
+                            </div>
+
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                </div>
+            </aside>
+        </div>
+    );
+}
